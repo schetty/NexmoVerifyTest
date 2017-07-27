a898529184b1f8431e4484212689dcc27f623c3a
Naomi Schettini <naomi@bluecart.com>
2017-07-27 00:51:59 -0700

    first commit, IB set up

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NexmoTest
+//
+//  Created by naomi schettini on 2017-07-25.
+//  Copyright © 2017 naomi schettini. All rights reserved.
+//
+
+import UIKit
+import NexmoVerify
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
