7f7ae5c2e88202ca06b57ae3423655a229a1be8a
Naomi Schettini <naomi@bluecart.com>
2017-08-07 00:35:20 -0700

    added getStatus function which contains a switch statement for retrieving the status of the user's verification

--- MainViewController.swift
+++ MainViewController.swift
@@ -17,11 +17,13 @@ class MainViewController: UIViewController, UITextViewDelegate {
     /** The UILabel which provides instructions to the user above the text field **/
     @IBOutlet weak var addPhoneNoOrPinLabel: UILabel!
     
+    /** The TextField for entering the user's phone number and Pin **/
+    @IBOutlet weak var phoneNoTF: UITextField!
+    
     /** The UILabel which appears below the text field **/
     @IBOutlet weak var successLabel: UILabel!
 
-    /** The TextField for entering the user's phone number and Pin **/
-    @IBOutlet weak var phoneNoTF: UITextField!
+
     
     
     //  #pragma mark : Variables
@@ -31,12 +33,14 @@ class MainViewController: UIViewController, UITextViewDelegate {
     
     // #pragma mark : IB ACTIONS
     @IBAction func didTapSend(_ sender: Any) {
-        self.isVerified = verifyUser()
-        if (isVerified == false) {
+        
+        getStatus()
+        
+        if (self.isVerified == false) {
             if ((phoneNoTF.text?.characters.count)! > 4) {
                 self.sendPhoneNumberToFirbase()
-                self.successLabel.text = "Sending you a Pin"
                 self.successLabel.isHidden = false
+                self.successLabel.text = "Sending you a Pin"
                 let result = self.verifyUser()
                 if (result == true) {
                     self.successLabel.text = "Please enter your pin"
@@ -59,6 +63,7 @@ class MainViewController: UIViewController, UITextViewDelegate {
         }
         
         else if (isVerified == true) {
+            self.successLabel.isHidden = false
             self.successLabel.text = "You've been verified already!"
             self.addPhoneNoOrPinLabel.isHidden = true
 
@@ -134,6 +139,131 @@ class MainViewController: UIViewController, UITextViewDelegate {
         return phoneStr
     }
     
+    /**
+        gets the user's verification status
+     
+     *verified*
+     The user has been successfully verified.
+     
+     *pending*
+     A verification request for this user
+     is currently in progress.
+     
+     *new*
+     This user just been created on the SDK service
+     
+     *failed*
+     A previous verification request for this
+     user has failed.
+     
+     *expired*
+     A previous verification request for this
+     user expired.
+     
+     *unverified*
+     A user's verified status has been revoked,
+     possibly due to timeout.
+     
+     *blacklisted*
+     This user has failed too many verification
+     requests and is therefore blacklisted.
+     
+     *error*
+     An error ocurred during the last verification
+     attempt for this user.
+     
+     *unknown*
+     The user is unknown to the SDK service.
+     **/
+    
+    func getStatus() {
+        
+        VerifyClient.getUserStatus(countryCode: "GB", number: self.phoneNoTF.text!) { status, error in
+            if let error = error {
+                // unable to get user status for given device + phone number pair
+                
+                let alert = UIAlertView()
+                alert.title = "Error"
+                alert.message = "Something went wrong"
+                alert.addButton(withTitle: "OK")
+                alert.show()
+                
+                return
+            }
+            switch (status!) {
+                
+            case "pending":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "pending"
+                self.addPhoneNoOrPinLabel.isHidden = false
+                self.addPhoneNoOrPinLabel.text = "enter your PIN when it arrives"
+                self.isVerified = false
+
+                
+            case "verified":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "verified!"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = true
+                
+            case "new":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "your number is new"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+                
+            case "failed":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "something went wrong"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+            case "expired":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "expired :("
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+            case "unverified":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "not yet verified!"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+            case "blacklisted":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "your number is not allowed"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+            case "error":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "error!"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+            case "unknown":
+                self.successLabel.isHidden = false
+                self.successLabel.text = "nexmo is sending you an SMS right now"
+                self.addPhoneNoOrPinLabel.isHidden = true
+                self.isVerified = false
+
+                
+            default:
+                print("did not execute")
+                
+            }
+            
+        }
+    }
+    
     
     /**
      Verifies user phone number and their pin number sent via sms 
@@ -142,7 +272,7 @@ class MainViewController: UIViewController, UITextViewDelegate {
     **/
     func verifyUser() -> Bool {
         let phoneNoToVerify = phoneNoTF.text
-        VerifyClient.getVerifiedUser(countryCode: "US", phoneNumber: phoneNoToVerify!,
+        VerifyClient.getVerifiedUser(countryCode: "GB", phoneNumber: phoneNoToVerify!,
                                      
                                      onVerifyInProgress: {
                                         // Called when the verification process begins.
@@ -168,7 +298,7 @@ class MainViewController: UIViewController, UITextViewDelegate {
 
         })
         
-        return isVerified == true
+        return isVerified
     }
     
 
