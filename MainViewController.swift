dc1761ba32e50c36e25480e77d4d4fa6bae0f412
Naomi Schettini <naomi@bluecart.com>
2017-08-06 23:57:08 -0700

    Added documentation to the MainViewController

--- MainViewController.swift
+++ MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NexmoTest
 //
 //  Created by naomi schettini on 2017-07-25.
@@ -10,31 +10,58 @@ import UIKit
 import NexmoVerify
 import Firebase
 
-class ViewController: UIViewController, UITextViewDelegate {
+class MainViewController: UIViewController, UITextViewDelegate {
     
-  //  #pragma mark : IB OUTLETS
+    //  #pragma mark : IB OUTLETS
     
-    @IBOutlet weak var phoneNoTF: UITextField!
-    @IBOutlet weak var successLabel: UILabel!
+    /** The UILabel which provides instructions to the user above the text field **/
     @IBOutlet weak var addPhoneNoOrPinLabel: UILabel!
+    
+    /** The UILabel which appears below the text field **/
+    @IBOutlet weak var successLabel: UILabel!
+
+    /** The TextField for entering the user's phone number and Pin **/
+    @IBOutlet weak var phoneNoTF: UITextField!
+    
+    
+    //  #pragma mark : Variables
+    var isVerified: Bool = false
+    
     
     
     // #pragma mark : IB ACTIONS
     @IBAction func didTapSend(_ sender: Any) {
-        if ((phoneNoTF.text?.characters.count)! > 4) {
-            sendPhoneNumberToFirbase()
-            successLabel.text = "Sending you a Pin"
-            successLabel.isHidden = false
-            let result = verifyUser()
-            if (result == true) {
-                successLabel.text = "Please enter your pin"
+        self.isVerified = verifyUser()
+        if (isVerified == false) {
+            if ((phoneNoTF.text?.characters.count)! > 4) {
+                self.sendPhoneNumberToFirbase()
+                self.successLabel.text = "Sending you a Pin"
+                self.successLabel.isHidden = false
+                let result = self.verifyUser()
+                if (result == true) {
+                    self.successLabel.text = "Please enter your pin"
+                }
+            }
+            else if (phoneNoTF.text?.characters.count == 4){
+                let response = VerifyClient.checkPinCode(phoneNoTF.text!)
+                if (response != nil) {
+                    successLabel.isHidden = false
+                }
+            }
+                
+            else {
+                let alert = UIAlertView()
+                alert.title = "Error"
+                alert.message = "Something went wrong"
+                alert.addButton(withTitle: "OK")
+                alert.show()
             }
         }
-        else if (phoneNoTF.text?.characters.count == 4){
-           let response = VerifyClient.checkPinCode(phoneNoTF.text!)
-            if (response != nil) {
-                successLabel.isHidden = false
-            }
+        
+        else if (isVerified == true) {
+            self.successLabel.text = "You've been verified already!"
+            self.addPhoneNoOrPinLabel.isHidden = true
+
         }
         
         else {
@@ -44,8 +71,9 @@ class ViewController: UIViewController, UITextViewDelegate {
             alert.addButton(withTitle: "OK")
             alert.show()
         }
+        
     }
-    
+            
     /**
         Clears the TextField after Editing
     **/
@@ -77,6 +105,16 @@ class ViewController: UIViewController, UITextViewDelegate {
         }
     }
     
+    /*
+     Sends isVerified bool to Firebase
+     */
+    func sendVerificationToFirbase() {
+        
+        ref =  Database.database().reference()
+        if (phoneNoTF.text != "") {
+            ref?.child("phoneNumbers").child(phoneNoTF.text!).child("isVerified").setValue(["isVerified": self.isVerified])
+        }
+    }
     
     /**
      Retrieves phone Numbers from FireBase
@@ -99,36 +137,38 @@ class ViewController: UIViewController, UITextViewDelegate {
     
     /**
      Verifies user phone number and their pin number sent via sms 
+     
+     return bool: if iterated through all cases of the VerifyUser methods, this means the user is already verified
     **/
     func verifyUser() -> Bool {
-        var isVerified = false
         let phoneNoToVerify = phoneNoTF.text
         VerifyClient.getVerifiedUser(countryCode: "US", phoneNumber: phoneNoToVerify!,
                                      
                                      onVerifyInProgress: {
                                         // Called when the verification process begins.
+                                        self.addPhoneNoOrPinLabel.text = "Enter Your Pin"
         },
                                      onUserVerified: {
                                         // Called when the user has been successfully verified.
-                                        
     
                                         self.addPhoneNoOrPinLabel.text = "Enter Your Pin"
                                         if (self.phoneNoTF.text != "") {
                                             VerifyClient.checkPinCode(self.addPhoneNoOrPinLabel.text!) }
-                                        isVerified = true
+                                        self.isVerified = true
+                                        self.sendVerificationToFirbase()
                                         
         },
                                      onError: { (error: VerifyError) in
                                         // Called when an error occurs during verification. For example, the user enters the wrong pin.
                                         // See the VerifyError class for more details.
                                         
-                                        isVerified = false
+                                        self.isVerified = false
 
                                         
 
         })
         
-        return isVerified
+        return isVerified == true
     }
     
 
